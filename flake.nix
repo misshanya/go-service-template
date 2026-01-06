095ce106506142f60bbdb05a988cfdf4576b467d
misshanya <mishanyadev@gmail.com>
2026-01-06 09:18:34 +0300

    feat: add nix dev shell

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        goVersion = pkgs.go_1_25;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            goVersion
+
+            gopls
+            gotools
+            delve
+            golangci-lint
+
+            air
+
+            sqlc
+            goose
+            go-swag
+          ];
+
+          shellHook = ''
+            export GOROOT="${goVersion}/share/go"
+          '';
+        };
+      }
+    );
+}
